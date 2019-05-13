6ab40b5f03c5130d8ee4982f804b9c92a4343d86
John Williams <johnnjit7@gmail.com>
2019-05-13 18:35:19 -0400

    Added .env file and gitignore file

--- routes/api.js
+++ routes/api.js
@@ -12,29 +12,30 @@ var expect = require('chai').expect;
 var MongoClient = require('mongodb');
 var ObjectId = require('mongodb').ObjectID;
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const MONGO_URI = process.env.MONGO_URI; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
   
     .get(function (req, res){
-      var project = req.params.project;
-      
+      let project = req.params.project;
+     
     })
     
     .post(function (req, res){
-      var project = req.params.project;
-      
+      let project = req.params.project;
+      //console.log('params: ' + JSON.stringify(req.query.issue_title));
+      console.log('params: ' + JSON.stringify(req.body));
     })
     
     .put(function (req, res){
-      var project = req.params.project;
+      let project = req.params.project;
       
     })
     
     .delete(function (req, res){
-      var project = req.params.project;
+      let project = req.params.project;
       
     });
     
