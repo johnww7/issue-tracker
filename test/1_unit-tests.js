d20ea157640765cc3196a377e50acabb33691466
John Williams <johnnjit7@gmail.com>
2019-05-20 21:05:13 -0400

    Trying to get TDD testing to work on sendIssue function

---
+++ test/1_unit-tests.js
@@ -0,0 +1,34 @@
+/*
+*
+*
+*       FILL IN EACH UNIT TEST BELOW COMPLETELY
+*       -----[Keep the tests in the same order!]----
+*       (if additional are added, keep them at the very end!)
+*/
+
+var chai = require('chai');
+var assert = chai.assert;
+var controller = require('../handler/controller.js');
+
+var issueController = new controller(); 
+
+describe('Unit Tests', function() {
+
+//none required
+    describe('Function issueController.sendIssue(data)', function(){
+        
+        test('All fields filled input', function(done) {
+            let inputData = {
+                issue_title: 'test',
+                issue_text: 'test data',
+                created_by: 'john',
+                assigned_to: 'joe',
+                status_text: 'In QA'
+            }
+            assert.isObject(issueController.sendIssue(inputData), 'Return value is an object');
+            done();
+        });
+        
+    });
+
+})
