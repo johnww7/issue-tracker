8e46c7830f34cb548d43966a06bb10a03a0315ce
John Williams <johnnjit7@gmail.com>
2019-07-26 18:28:10 -0400

    WOrking on delete route

--- handler/controller.js
+++ handler/controller.js
@@ -1,74 +1,71 @@
-const shortid = require('shortid');
+const shortid = require("shortid");
 
 function IssueController() {
-    this.sendIssue = function(data) {
-        console.log("contents of issue: " + JSON.stringify(data));
+  this.sendIssue = function(data) {
+    console.log("contents of issue: " + JSON.stringify(data));
 
-        let created_on = new Date();
-        let updated_on = new Date();
-        let open = true;
-        let returnData;
+    let created_on = new Date();
+    let updated_on = new Date();
+    let open = true;
+    let returnData;
 
-        if(data.issue_title === '') {
-            return returnData = {
-                issue_title: 'Please fill out title field'
-            };
-        }
-        else if(data.issue_text === '') {
-            return returnData = {
-                issue_text:'Please fill out text field'
-            };
-        }
-        else if(data.created_by === '') {
-            return returnData = {
-                created_by:'Please fill out created by field'
-            };
-        }
-        else {
-            //_id: shortid.generate(),
-            return returnData = {
-                
-                issue_title: data.issue_title,
-                issue_text: data.issue_text,
-                created_on: new Date(),
-                updated_on: new Date(),
-                created_by: data.created_by,
-                assigned_to: data.assigned_to,
-                open: true,
-                status_text: data.status_text,
-            };
-        }
+    if (data.issue_title === "") {
+      return (returnData = {
+        issue_title: "Please fill out title field"
+      });
+    } else if (data.issue_text === "") {
+      return (returnData = {
+        issue_text: "Please fill out text field"
+      });
+    } else if (data.created_by === "") {
+      return (returnData = {
+        created_by: "Please fill out created by field"
+      });
+    } else {
+      //_id: shortid.generate(),
+      return (returnData = {
+        issue_title: data.issue_title,
+        issue_text: data.issue_text,
+        created_on: new Date(),
+        updated_on: new Date(),
+        created_by: data.created_by,
+        assigned_to: data.assigned_to,
+        open: true,
+        status_text: data.status_text
+      });
+    }
 
-        //return issue;
-    };
+    //return issue;
+  };
 
-    
-    this.updateIssue = function(entryData) {
-        let updated_on = {updated_on: new Date()};
-        let open = {open: (entryData.open || true)};
-        console.log('Update data from issue: ' + JSON.stringify(entryData));
+  this.updateIssue = function(entryData) {
+    let updated_on = { updated_on: new Date() };
+    let open = { open: entryData.open || true };
+    console.log("Update data from issue: " + JSON.stringify(entryData));
 
-        let filterUpdateData = Object.keys(entryData).filter(function(key) {
-            return entryData[key] !== '' && key !== '_id';
-        }).map(function(key) {
-            return { [key]: entryData[key]};
-        }).reduce(function(acc, curr) {
-            let fieldKey = Object.keys(curr)[0];
-            acc[fieldKey] = curr[fieldKey];
-            return acc;
-        }, {});
+    let filterUpdateData = Object.keys(entryData)
+      .filter(function(key) {
+        return entryData[key] !== "" && key !== "_id";
+      })
+      .map(function(key) {
+        return { [key]: entryData[key] };
+      })
+      .reduce(function(acc, curr) {
+        let fieldKey = Object.keys(curr)[0];
+        acc[fieldKey] = curr[fieldKey];
+        return acc;
+      }, {});
 
-        console.log('filtered fields: ' + JSON.stringify(filterUpdateData));
-        if(Object.keys(filterUpdateData).length == 0) {
-            return {fail: 'no fields'};
-        }
-        else {
-            let updateData = Object.assign({}, filterUpdateData, updated_on, open);
-            //let updateData = {$set: update};
-            console.log('Sending for update' + JSON.stringify(updateData));
-            return updateData;
-        }
-    };
+    console.log("filtered fields: " + JSON.stringify(filterUpdateData));
+    if (Object.keys(filterUpdateData).length == 0) {
+      return { fail: "no fields" };
+    } else {
+      let updateData = Object.assign({}, filterUpdateData, updated_on, open);
+      //let updateData = {$set: update};
+      console.log("Sending for update" + JSON.stringify(updateData));
+      return updateData;
+    }
+  };
 }
 
 module.exports = IssueController;
--- routes/api.js
+++ routes/api.js
@@ -1,215 +1,234 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectID;
-var Controller = require('../handler/controller.js');
+var expect = require("chai").expect;
+var MongoClient = require("mongodb").MongoClient;
+var ObjectId = require("mongodb").ObjectID;
+var Controller = require("../handler/controller.js");
 
-  const MONGO_URI = process.env.MONGO_URI; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const MONGO_URI = process.env.MONGO_URI; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
- var issueController = new Controller();
+var issueController = new Controller();
 
-module.exports = function (app) {
-  
-      //const collection = db.collection(project);
-      app.route('/api/issues/:project')      
-        .get(function (req, res){
-          let project = req.params.project;
-          console.log('Get: ' + project);
-        })
-        
-        .post(function (req, res){
-          let project = req.params.project;
-          let issueData = req.body;
-          //console.log('params: ' + JSON.stringify(req.query.issue_title));
-          console.log('params: ' + JSON.stringify(req.body));
+module.exports = function(app) {
+  //const collection = db.collection(project);
+  app
+    .route("/api/issues/:project")
+    .get(function(req, res) {
+      let project = req.params.project;
+      console.log("Get: " + project);
+    })
 
-          let submitIssue = issueController.sendIssue(issueData);
-          console.log('submitted issue: ' + JSON.stringify(submitIssue));
+    .post(function(req, res) {
+      let project = req.params.project;
+      let issueData = req.body;
+      //console.log('params: ' + JSON.stringify(req.query.issue_title));
+      console.log("params: " + JSON.stringify(req.body));
 
-          if(Object.keys(submitIssue).length === 1 ) {
-            res.json(submitIssue);
-          }
-          else{
-            let issueInserted;
-            try {
-              MongoClient.connect(MONGO_URI, (err, db) => {
-                if(err) {
-                  console.log('Database error: ' + err);
-                }
-                console.log('Successful database connection!');
-                let myPromise = () => {
-                  return new Promise((resolve, reject) => {
-                    db.collection(project).insertOne(submitIssue, (err, res) => {
-                      if (err) { 
-                        reject(err); 
-                      }
-                      else {
-                        console.log("1 issue inserted");
-                        issueInserted = res.ops[0]._id;
-                        console.log('id: ' + res.ops[0]._id);
-                        resolve(issueInserted);
-                      }
-                    });
-                  });
-                };
-                let insertPromise = async() => { 
-                  let result = await (myPromise());
-                  return result;
-                
-                };
-                
-                insertPromise().then(function(promResult) {
-                  db.close();
-                  let postedIssue = Object.assign({}, {_id: promResult}, submitIssue);
-                  console.log('posted: ' + JSON.stringify(postedIssue));
-                  //console.log('posted: ' + JSON.stringify(promResult));
-                  //res.json(promResult);
-                  res.json(postedIssue);
-                });
-                
-                  
-              });
-            } catch(e) {
-              next(e)
+      let submitIssue = issueController.sendIssue(issueData);
+      console.log("submitted issue: " + JSON.stringify(submitIssue));
+
+      if (Object.keys(submitIssue).length === 1) {
+        res.json(submitIssue);
+      } else {
+        let issueInserted;
+        try {
+          MongoClient.connect(MONGO_URI, (err, db) => {
+            if (err) {
+              console.log("Database error: " + err);
             }
-            
-          }
-        })
-        
-        .put(function (req, res) {
-          let project = req.params.project;
-          let issueData = req.body;
-          let idSearch = issueData._id;
-          console.log('Put: ' + project);
-          console.log('put data: ' + JSON.stringify(issueData));
+            console.log("Successful database connection!");
+            let myPromise = () => {
+              return new Promise((resolve, reject) => {
+                db.collection(project).insertOne(submitIssue, (err, res) => {
+                  if (err) {
+                    reject(err);
+                  } else {
+                    console.log("1 issue inserted");
+                    issueInserted = res.ops[0]._id;
+                    console.log("id: " + res.ops[0]._id);
+                    resolve(issueInserted);
+                  }
+                });
+              });
+            };
+            let insertPromise = async () => {
+              let result = await myPromise();
+              return result;
+            };
 
-          let result;
-          try {
-            MongoClient.connect(MONGO_URI, (err, db) => {
-              if(err) {
-                console.log('Database error: ' + err);
-              }
-              
-              console.log('Successful database connection!');
-              let updateDataToSend = issueController.updateIssue(issueData);
-              console.log('updateData type: ' + typeof(updateDataToSend));
-              let myPromise = () => {
-                return new Promise((resolve, reject) => {
-                  if(updateDataToSend.fail === 'no fields'){
-                    resolve({matchedCount: 1, modifiedCount: 0})
-                  }
-                  else if(!ObjectId.isValid(req.body._id)){
-                    resolve({matchedCount: 0, modifiedCount: 0});
-                  }
-                  else {
-                    
-                    db.collection(project).updateOne({"_id": ObjectId(req.body._id)}, {$set: updateDataToSend}, (err, data) => {
-                      if(err) {
-                        reject(err);
-                      }
-                      else {
-                    
-                      console.log("Updated: " + JSON.stringify(data));         
-                      console.log('1 updated occured');
+            insertPromise().then(function(promResult) {
+              db.close();
+              let postedIssue = Object.assign(
+                {},
+                { _id: promResult },
+                submitIssue
+              );
+              console.log("posted: " + JSON.stringify(postedIssue));
+              //console.log('posted: ' + JSON.stringify(promResult));
+              //res.json(promResult);
+              res.json(postedIssue);
+            });
+          });
+        } catch (e) {
+          next(e);
+        }
+      }
+    })
+
+    .put(function(req, res) {
+      let project = req.params.project;
+      let issueData = req.body;
+      let idSearch = issueData._id;
+      console.log("Put: " + project);
+      console.log("put data: " + JSON.stringify(issueData));
+
+      let result;
+      try {
+        MongoClient.connect(MONGO_URI, (err, db) => {
+          if (err) {
+            console.log("Database error: " + err);
+          }
+
+          console.log("Successful database connection!");
+          let updateDataToSend = issueController.updateIssue(issueData);
+          console.log("updateData type: " + typeof updateDataToSend);
+          let myPromise = () => {
+            return new Promise((resolve, reject) => {
+              if (updateDataToSend.fail === "no fields") {
+                resolve({ matchedCount: 1, modifiedCount: 0 });
+              } else if (!ObjectId.isValid(req.body._id)) {
+                resolve({ matchedCount: 0, modifiedCount: 0 });
+              } else {
+                db.collection(project).updateOne(
+                  { _id: ObjectId(req.body._id) },
+                  { $set: updateDataToSend },
+                  (err, data) => {
+                    if (err) {
+                      reject(err);
+                    } else {
+                      console.log("Updated: " + JSON.stringify(data));
+                      console.log("1 updated occured");
 
                       resolve(data);
-                      }
-                    });
+                    }
                   }
-                });
-              };
-
-              let updatePromise = async() => { 
-                let resultUpdate = await (myPromise());
-                console.log('returned data: ' + resultUpdate);
-                return resultUpdate;
-              };
-              
-              updatePromise().then(function(promResult) {
-                db.close();
-                console.log('update result: ' + promResult.matchedCount + ' modified: ' + promResult.modifiedCount);
-                let updateResult;
-                if(promResult.matchedCount == 1 && promResult.modifiedCount == 1){
-                  updateResult = {result: 'successfully updated'};
-                }
-                else if(promResult.matchedCount == 1 && promResult.modifiedCount == 0) {
-                  updateResult = {result: 'no updated field sent'};
-                }
-                else {
-                  updateResult = {result: 'could not update ' + issueData._id};
-                }
-
-                res.json(updateResult);
-              });                
-            });
-          }
-          catch (e) {
-            next(e);
-          }
-           
-             
-        })
-        
-        .delete(function (req, res){
-          let project = req.params.project;
-          let deleteIssue = req.body;
-          let deleteResult;
-          console.log('Delete: ' + project);
-          console.log('Issue to delete: ' + JSON.stringify(deleteIssue));
-
-          
-          if(deleteIssue._id === '' || deleteIssue._id === null) {
-            res.json({result: '_id error'});
-          }
-          else {
-            MongoClient.connect(MONGO_URI, (err, db) => {
-              if(err) {
-                console.log('Database error: ' + err);
+                );
               }
-              console.log('Successful database connection!');
-              let deleteQuery = {_id: ObjectId(deleteIssue._id)};
-              db.collection(project).deleteOne(deleteIssue._id, function(err,resObj) {
-                if(err) { console.log(err); }
-
-                if (resObj.deletedCount == 1 || resObj.deletedCount === '1'){
-                  console.log('deleted ' + deleteIssue._id);
-                  deleteResult = 1;
-                 //deleteResult = {success: 'deleted ' + deleteIssue._id};
-                  
-                }
-                else {
-                  console.log('could not delete ' + deleteIssue._id);
-                  deleteResult = 0;
-                  //deleteResult = {failed: 'could not delete ' + deleteIssue._id};
-                  
-                }
-                db.close();
-              });
             });
-            //res.json({succes: 'success deleted'});
-            if(deleteResult = 1){
-              let success = 'deleted ' + deleteIssue._id;
-              res.json({success});
-              
+          };
+
+          let updatePromise = async () => {
+            let resultUpdate = await myPromise();
+            console.log("returned data: " + resultUpdate);
+            return resultUpdate;
+          };
+
+          updatePromise().then(function(promResult) {
+            db.close();
+            console.log(
+              "update result: " +
+                promResult.matchedCount +
+                " modified: " +
+                promResult.modifiedCount
+            );
+            let updateResult;
+            if (promResult.matchedCount == 1 && promResult.modifiedCount == 1) {
+              updateResult = { result: "successfully updated" };
+            } else if (
+              promResult.matchedCount == 1 &&
+              promResult.modifiedCount == 0
+            ) {
+              updateResult = { result: "no updated field sent" };
+            } else {
+              updateResult = { result: "could not update " + issueData._id };
+            }
+
+            res.json(updateResult);
+          });
+        });
+      } catch (e) {
+        next(e);
+      }
+    })
+
+    .delete(function(req, res) {
+      let project = req.params.project;
+      let deleteIssue = req.body;
+      let deleteResult;
+      console.log("Delete: " + project);
+      console.log("Issue to delete: " + JSON.stringify(deleteIssue));
+      try {
+        MongoClient.connect(MONGO_URI, (err, db) => {
+          if (err) {
+            console.log("Database error: " + err);
+          }
+          console.log("Successful database connection!");
+          let myPromise = () => {
+            return new Promise((resolve, reject) => {
+              if (!ObjectId.isValid(req.body._id)) {
+                resolve({ result: "_id error" });
+              } else {
+                let deleteQuery = { _id: ObjectId(deleteIssue._id) };
+                db.collection(project).deleteOne(deleteIssue._id, function(
+                  err,
+                  resObj
+                ) {
+                  if (err) {
+                    console.log(err);
+                  }
+                  else {
+                    resolve(resObj);
+                  }
+
+                });
+              }
+            });
+          };
+          let deletePromise = async () => {
+            let deleteResult = await myPromise();
+            console.log("deleted data: " + deleteResult);
+            return deleteResult;
+          };
+
+          deletePromise().then(function(promResult){
+            db.close();
+            let result;
+            if(promResult.deleteCount == 1) {
+              result = {success: 'deleted ' + deleteIssue._id};
+            }
+            else if(promResult.deleteCount == 0) {
+              result = {failed: 'could not delete' + deleteIssue._id};
             }
             else {
-              let failed = 'could not delete ' + deleteIssue._id;
-              res.json({failed});
+              result = promResult;
             }
-            
-          }
+            res.json(result);
+          });
 
+          db.close();
         });
-    
-   
+      } catch (e) {
+        next(e);
+      }
+      /*if (deleteIssue._id === "" || deleteIssue._id === null) {
+        res.json({ result: "_id error" });
+      } else {
+        //res.json({succes: 'success deleted'});
+        if ((deleteResult = 1)) {
+          let success = "deleted " + deleteIssue._id;
+          res.json({ success });
+        } else {
+          let failed = "could not delete " + deleteIssue._id;
+          res.json({ failed });
+        }
+      }*/
+    });
 };
-
